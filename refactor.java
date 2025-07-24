2324ba3610864c6e440d417819aebf6c8fbdcd9e
pvthach25 <thachpham2005cm@gmail.com>
2025-07-24 14:30:10 +0700

    Bước 4: Tách hàm kiểm tra nhiệm vụ trùng lặp

--- refactor.java
+++ refactor.java
@@ -88,8 +88,7 @@ public class PersonalTaskManagerViolations {
         // Kiểm tra trùng lặp
         for (Object obj : tasks) {
             JSONObject existingTask = (JSONObject) obj;
-            if (existingTask.get("title").toString().equalsIgnoreCase(title) &&
-                existingTask.get("due_date").toString().equals(dueDate.format(DATE_FORMATTER))) {
+            if (isDuplicateTask(tasks, title, dueDate.format(DATE_FORMATTER))) {
                 System.out.println(String.format("Lỗi: Nhiệm vụ '%s' đã tồn tại với cùng ngày đến hạn.", title));
                 return null;
             }
